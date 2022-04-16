c9b88113a1e545ecb8d873a1e93111ad5a400ced
Martin Heywang <martin.heywang1@gmail.com>
2022-04-16 11:01:52 +0200

    Initialized (hash) routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 import "./index.scss";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <HashRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </HashRouter>,
     document.getElementById("root")
 );
